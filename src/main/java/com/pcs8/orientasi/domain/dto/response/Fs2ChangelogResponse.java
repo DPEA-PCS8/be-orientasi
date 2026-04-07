bfc358a030cbf9f09c6b08bfb5dfce96716c535e
Gebby Geovany <92412757+gebbygeovany99@users.noreply.github.com>
2026-04-07 08:33:27 +0700

    refactor: reduce code duplication in changelog responses
    - Create BaseChangelogResponse abstract class with shared fields
    - Update PksiChangelogResponse to extend BaseChangelogResponse
    - Update Fs2ChangelogResponse to extend BaseChangelogResponse
    - Reduces duplication from 93.3% to 0% for SonarQube compliance
    - Fixes: Duplicated Lines (%) on New Code > 3%

--- src/main/java/com/pcs8/orientasi/domain/dto/response/Fs2ChangelogResponse.java
+++ src/main/java/com/pcs8/orientasi/domain/dto/response/Fs2ChangelogResponse.java
@@ -1,39 +1,19 @@
 package com.pcs8.orientasi.domain.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
-
+/**
+ * Response for FS2 document changelog.
+ * Extends BaseChangelogResponse to reduce code duplication.
+ */
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
-public class Fs2ChangelogResponse {
-
-    private String id;
-
-    @JsonProperty("field_name")
-    private String fieldName;
-
-    @JsonProperty("field_label")
-    private String fieldLabel;
-
-    @JsonProperty("old_value")
-    private String oldValue;
-
-    @JsonProperty("new_value")
-    private String newValue;
-
-    @JsonProperty("updated_by")
-    private String updatedBy;
-
-    @JsonProperty("updated_by_name")
-    private String updatedByName;
-
-    @JsonProperty("updated_at")
-    private LocalDateTime updatedAt;
+@EqualsAndHashCode(callSuper = true)
+public class Fs2ChangelogResponse extends BaseChangelogResponse {
+    // All fields are inherited from BaseChangelogResponse
+    // No additional fields needed
 }
