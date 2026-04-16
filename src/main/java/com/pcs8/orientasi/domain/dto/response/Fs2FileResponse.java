d8f423689af28ca89eac44d6906a53fe743ba215
Gebby Geovany <92412757+gebbygeovany99@users.noreply.github.com>
2026-04-16 11:05:09 +0700

    add date to file upload (#158)
    * add date to file upload
    
    * fix code duplication

--- src/main/java/com/pcs8/orientasi/domain/dto/response/Fs2FileResponse.java
+++ src/main/java/com/pcs8/orientasi/domain/dto/response/Fs2FileResponse.java
@@ -1,32 +1,18 @@
 package com.pcs8.orientasi.domain.dto.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Fs2FileResponse {
-    private UUID id;
+@EqualsAndHashCode(callSuper = true)
+public class Fs2FileResponse extends BaseFileResponse {
     private UUID fs2Id;
-    private String fileName;
-    private String originalName;
-    private String contentType;
-    private Long fileSize;
-    private String blobUrl;
-    private String fileType; // ND, FS2, CD, FS2A, FS2B, F45, F46, NDBA
-    private LocalDateTime createdAt;
-    private Integer version;
-    private UUID fileGroupId;
-    private String displayName;
-    private boolean isLatestVersion;
 }
