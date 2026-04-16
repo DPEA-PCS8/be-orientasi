86250d5897e451d5e3e39c16433cf8c693076362
Mohammad Abuemas Rizq Wijaya <mrizqwijaya@gmail.com>
2026-04-16 08:15:15 +0700

    Update FS2 (Filter, Progress Style, Date, etc) - BE (#156)

--- src/main/java/com/pcs8/orientasi/service/Fs2Service.java
+++ src/main/java/com/pcs8/orientasi/service/Fs2Service.java
@@ -16,7 +16,7 @@ public interface Fs2Service {
 
     List<Fs2DocumentResponse> getAll();
 
-    Page<Fs2DocumentResponse> search(String search, UUID bidangId, UUID skpaId, String status, Integer year, Integer startMonth, Integer endMonth, Pageable pageable, String userDepartment, boolean canSeeAll);
+    Page<Fs2DocumentResponse> search(String search, UUID aplikasiId, String statusTahapan, UUID skpaId, String status, Integer year, Integer startMonth, Integer endMonth, Pageable pageable, String userDepartment, boolean canSeeAll);
 
     List<Fs2DocumentResponse> searchList(String search, UUID bidangId, UUID skpaId, String status, String userDepartment, boolean canSeeAll);
 
